36fb634f08c026e7bd781457c2eb2f5ebdee3ec4
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-10-06 09:15:10 -0300

    Cadastro e Exibição de Interesses

--- PI/recepcao/paginas/cadastro/interesses.php
+++ PI/recepcao/paginas/cadastro/interesses.php
@@ -4,12 +4,13 @@
 <hr>
 <?php 
     $conexao = new PDO("mysql:dbname=recepcao;host=localhost","root","");
-    $selectcursos = $conexao->PREPARE("SELECT cursos.*, categoria.nome as 'categoria', categoria.modalidade as 'modalidade' from cursos join categoria on cursos.categoria_id = categoria.id");
+    $selectcursos = $conexao->PREPARE("SELECT cursos.*, categoria.interessado as 'categoria', categoria.modalidade as 'modalidade' from cursos join categoria on cursos.categoria_id = categoria.id");
     $selectcursos->execute();
     $cursos = $selectcursos->fetchAll(PDO::FETCH_ASSOC);
-    $selectinteressados = $conexao->PREPARE("SELECT * from interessados");
+    $selectinteressados = $conexao->PREPARE("SELECT ci.* ,inte.contato ,inte.email ,inte.escolaridade ,inte.dtNasc as 'nascimento' ,inte.tpcontato as 'tipocontato' ,inte.nome as 'interessado' ,cur.nome as 'curso' ,cur.dtIni as 'inicio' ,cur.dtFim as 'fim' ,cur.cargaHoraria as 'ch' ,capacidade ,cat.nome as 'categoria' ,modalidade from cursosinteressados ci join interessados inte on ci.interessados_id = inte.id join cursos cur on cur.id = ci.cursos_id join categoria cat on cur.categoria_id = cat.id");
     $selectinteressados->execute();
     $interessados = $selectinteressados->fetchAll(PDO::FETCH_ASSOC);
+    //var_dump($interessados);
 ?>
 <div style="display: flex; justify-content:center;">
     <form method="POST">    
@@ -21,7 +22,7 @@
                 foreach ($cursos as $c){
                     print 
                         "<option value='".$c['id']."' style='width:100%;'>".
-                            $c['nome']." - ".$c['categoria']." - ".$c['modalidade'].
+                            $c['interessado']." - ".$c['categoria']." - ".$c['modalidade'].
                         "</option>";                   
                 }
             ?>
@@ -32,7 +33,7 @@
                 foreach ($interessados as $i){
                     print 
                         "<option value='".$i['id']."' style='width:100%;'>".
-                            $i['nome'].
+                            $i['interessado'].
                         "</option>";                   
                 }
             ?>
@@ -46,7 +47,7 @@
 <hr>
 <table style="width:100%">
     <thead>
-        <tr style="font-weight:900;">
+        <tr style="font-weight:900; text-align:center;">
             <td>id</td>            
             <td>Categoria/Modalidade</td>
             <td>Curso</td>
@@ -56,13 +57,31 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>valor</td>
-            <td>valor</td>
-            <td>valor</td>
-            <td>valor</td>
-            <td>valor</td>
-            <td>valor</td>
-        </tr>
+        <?php
+            foreach ($interessados as $i){
+                print"
+                    <tr style='text-align:center;'>
+                        <td>"
+                            .$i['cursos_id']." - ".$i['interessados_id'].
+                        "</td>
+                        <td>"
+                            .$i['categoria']." / ".$i['modalidade'].
+                        "</td>
+                        <td>"
+                            .$i['curso'].
+                        "</td>
+                        <td>"
+                            .$i['interessado'].
+                        "</td>
+                        <td>"
+                            .$i['contato'].
+                        "</td>
+                        <td>"
+                            .$i['email'].
+                        "</td>
+                    </tr>
+                    ";
+            }
+        ?>
     </tbody>
 </table>
--- PI/recepcao/paginas/sobre.php
+++ PI/recepcao/paginas/sobre.php
@@ -1,6 +1,13 @@
-<h1>
-    Sobre
-</h1>
-<div style="width:600px; margin:auto;">
-    <img src="img/programadores.jpeg" style="width:500px;">
-</div>
+<div style="display:block!important;">
+    <h1 style="text-align:center;">
+        Sobre
+    </h1>
+    <hr>
+    <div style="width:600px; margin:auto; display:flex; justify-content:center;">
+        <img src="img/programadores.jpeg" style="width:500px;">
+    </div>
+    <hr>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam repellendus labore nam ipsa eum enim necessitatibus quas, maiores expedita consequatur omnis suscipit voluptatem, natus deserunt, amet iusto tempora vitae sed!
+    </p>
+</div> 
