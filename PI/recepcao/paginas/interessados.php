5cc9ce6fb98597ef000af79755ce02a00733661e
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-10-06 11:49:26 -0300

    Inicio de Construção da Página Interessados geral

--- PI/recepcao/paginas/interessados.php
+++ PI/recepcao/paginas/interessados.php
@@ -1,3 +1,58 @@
-<h1 style="color: gray;">
-    Interessados
-</h1>
+<style>
+    .caixa{
+        border: 1px solid gray;
+        border-radius:10px;
+        margin:2px;    
+        margin-top:10px!important;
+        margin-bottom:10px!important;       
+    }
+    a{
+        text-decoration:none;
+        color:gray;
+        padding:5px;
+        margin-top:10px!important;
+        margin-bottom:10px!important;
+        text-transform:uppercase;
+        font-size:0.8em;
+    }
+
+</style>
+
+<?php
+    $conexao = new PDO("mysql:dbname=recepcao;host=localhost","root","");
+    $selectcategoria = $conexao->PREPARE("SELECT * FROM categoria");
+    $selectcategoria->execute();
+    $categorias = $selectcategoria->fetchAll(PDO::FETCH_ASSOC);
+    $selectci = $conexao->PREPARE("SELECT ci.* ,inte.contato ,inte.email ,inte.escolaridade ,inte.dtNasc as 'nascimento' ,inte.tpcontato as 'tipocontato' ,inte.nome as 'interessado' ,cur.nome as 'curso' ,cur.dtIni as 'inicio' ,cur.dtFim as 'fim' ,cur.cargaHoraria as 'ch' ,capacidade ,cat.nome as 'categoria' ,modalidade from cursosinteressados ci join interessados inte on ci.interessados_id = inte.id join cursos cur on cur.id = ci.cursos_id join categoria cat on cur.categoria_id = cat.id");
+    $selectci->execute();
+    $cursointeressados = $selectci->fetchAll(PDO::FETCH_ASSOC);
+?>
+<div style="border:gray solid 2px; border-radius:15px; border-shadow 5px 5px black; width: 80vw; text-align:center; padding:5px;">
+    <h1 style="color: gray;">
+        Interessados
+        <sup>
+            <small>
+                Selecione Categoria/Modalidade :: Cursos >>>  visualizar
+            </small>
+        </sup>
+    </h1>
+    <hr>
+    <?php
+       // var_dump($categorias);
+        $contador = 0;
+        foreach($categorias as $c){
+            print"
+                    <a href='?pagina=interessados&categoria='".$c['id']."' class='caixa'>".
+                        $c['nome']." - ".$c['modalidade']
+                    ."</a>
+                ";
+                $contador++;
+                if($contador == 4){
+                    print "<br><br>";
+                    $contador=0;
+                }
+        }
+    ?>
+</div>
+
+
