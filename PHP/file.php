22eb05f08dd3ab8bb8de8c4ab9d37dd3ea14265b
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-10-18 11:48:09 -0300

    Session e Manipulação de Arquivos PHP

--- PHP/file.php
+++ PHP/file.php
@@ -1,1 +1,42 @@
+<form method="POST" enctype="multipart/form-data">
+    <input type="file" name="arquivo">
+    <button type="submit">Enviar</button>
+</form>
 
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+      $file = $_FILES["arquivo"];           
+     // var_dump($file);
+
+    if ($file["error"]) {
+
+        throw new Exception("Error: ".$file["error"]);
+
+    }
+
+    $diretorio = "uploads";
+    // Verifica se o diretorio existe
+    if (!is_dir($diretorio)) {
+        // não existindo cria o diretorio
+        mkdir($diretorio);
+
+    }
+
+   //if (move_uploaded_file($file["tmp_name"], $diretorio . DIRECTORY_SEPARATOR . $file["name"])) {
+   //if (move_uploaded_file($file["tmp_name"], "uploads/ehoempresario.exe")) {
+   $hora = time(); 
+   if (move_uploaded_file($file["tmp_name"], $diretorio . DIRECTORY_SEPARATOR . $hora ."_". $file["name"])) {
+
+        echo "Arquivo arribado com sucesso!";
+
+   } else {
+
+        throw new Exception("Não foi possível arribar o arquivo.");
+
+   }
+
+}
+
+?>
---
+++ PHP/sessao.php
@@ -0,0 +1,82 @@
+<?php
+
+session_start();
+
+//session_destroy();
+
+echo session_save_path();
+
+echo "<hr>";
+
+echo session_id();
+
+echo "<hr>";
+
+$_SESSION["nome"] = "SENAC - Programador de Sistemas";
+
+$_SESSION["usuario"] = "oempresario";
+
+$_SESSION["acessoem"] = time();
+
+echo "<br>";
+
+//session_unset($_SESSION['nome']);
+
+echo $_SESSION['nome'];
+
+echo "<br>";
+
+echo $_SESSION['acessoem'];
+
+echo "<hr>";
+
+//session_destroy();
+
+echo session_id();
+
+echo "<hr>";
+
+echo session_id();
+
+session_id('ofkncku49jq860tn0et5utgnkl');
+
+echo "<hr>";
+
+echo "novo hash <br>";
+echo session_id();
+
+echo "<hr>";
+
+var_dump(session_status());
+
+echo "<hr>";
+
+//session_abort();
+//session_destroy();
+
+switch(session_status()) {
+
+    case PHP_SESSION_DISABLED:
+    echo "Sessões desabilitadas";
+    break;
+
+    case PHP_SESSION_NONE:
+    echo "Sessões habilitadas, mas não existem";
+    break;
+
+    case PHP_SESSION_ACTIVE:
+    echo "Sessões habilitadas e existem";
+    break;
+
+}
+
+session_regenerate_id();
+
+echo "<hr>";
+
+echo session_id();
+
+echo "<hr>";
+
+var_dump($_SESSION);
+?>
