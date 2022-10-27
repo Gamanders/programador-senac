0b3c3182df5f12df57eea37f2c6a0361e573c706
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-10-27 12:01:24 -0300

    Controle de Acesso ao benefícios de Canhotin

--- PI/recepcao/config.php
+++ PI/recepcao/config.php
@@ -1,4 +1,5 @@
 <?php
+    $conexao = new PDO("mysql:dbname=recepcao;host=localhost","root","");
     if(session_status()==PHP_SESSION_NONE){  
         session_start();
     }
--- PI/recepcao/paginas/interessados.php
+++ PI/recepcao/paginas/interessados.php
@@ -1,3 +1,4 @@
+
 <style>
     .caixa{
         border: 1px solid gray;
@@ -18,14 +19,19 @@
 
 </style>
 
-<?php
-    $conexao = new PDO("mysql:dbname=recepcao;host=localhost","root","");
+<?php    
     $selectcategoria = $conexao->PREPARE("SELECT * FROM categoria");
     $selectcategoria->execute();
     $categorias = $selectcategoria->fetchAll(PDO::FETCH_ASSOC);
     $selectci = $conexao->PREPARE("SELECT ci.* ,inte.contato ,inte.email ,inte.escolaridade ,inte.dtNasc as 'nascimento' ,inte.tpcontato as 'tipocontato' ,inte.nome as 'interessado' ,cur.nome as 'curso' ,cur.dtIni as 'inicio' ,cur.dtFim as 'fim' ,cur.cargaHoraria as 'ch' ,capacidade ,cat.nome as 'categoria' ,modalidade from cursosinteressados ci join interessados inte on ci.interessados_id = inte.id join cursos cur on cur.id = ci.cursos_id join categoria cat on cur.categoria_id = cat.id");
     $selectci->execute();
     $cursointeressados = $selectci->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+    if(isset($_SESSION["usuario"])){
+        $tipo = $_SESSION["tipo"];        
+        if($tipo == "admin"){
 ?>
 <div style="border:gray solid 2px; border-radius:15px; border-shadow 5px 5px black; width: 80vw; text-align:center; padding:5px;">
     <h1 style="color: gray;">
@@ -54,5 +60,16 @@
         }
     ?>
 </div>
+<?php            
+        }
+    }
+    else{
+        print 
+        
+        "<h1 style='color:red; text-align:center'>
+            Sejá empresário da dança pra ter acesso aos privilegios que só Canhotin oferece
+        </h1>";
+    }
+?>
 
 
